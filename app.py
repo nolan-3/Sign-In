ba070fa75c9aa2ad0eed1caac03ae90b535bb923
Nolan McCloskey <nolamccl@haverford.org>
2023-03-12 17:08:09 -0400

    .

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__, static_url_path='', static_folder='static',)
 TIMEZONE = timezone("America/New_York")
 
 OPEN_TIME = datetime.time(7, 0)
-CLOSE_TIME = datetime.time(15, 40)
+CLOSE_TIME = datetime.time(17, 15)
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
